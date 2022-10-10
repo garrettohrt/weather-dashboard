b3c2c8eb10bb862cedb784620f7a82e6bc5281a6
gmun <garrettohrt@gmail.com>
2022-10-09 21:23:35 -0500

    add fetch api url's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
-var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=6a7eff456050fd1c3c03b704bec2225b"
+var searchButton = document.querySelector("#search-button")
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=6a7eff456050fd1c3c03b704bec2225b&units=imperial"
+
+var cityName = document.querySelector("#cityName")
 
 fetch(apiUrl).then(function(response) {
 if (response.ok) {
     response.json().then(function (data) {
-console.log(data)
+     console.log(data)
     })
 }
 })
+    var futApiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Minneapolis&appid=6a7eff456050fd1c3c03b704bec2225b&units=imperial"
+
+    fetch(futApiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log (data)
+            })
+        }
+    })
