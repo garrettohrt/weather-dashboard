d5b0b6c2f18c21b35a044057e93adafe8db107e3
gmun <garrettohrt@gmail.com>
2022-10-11 15:27:33 -0500

    finish local storage, refactor functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,16 @@
 var apiKey = "6a7eff456050fd1c3c03b704bec2225b"
 var searchButtonEl = document.querySelector("#search-button")
-var cityListEl = document.querySelector("city-storage")
+var cityListEl = document.querySelector("#city-storage")
 var currentCityEl = document.querySelector("#city-name")
 var currentTempEl = document.querySelector("#temp")
 var currentWindEl = document.querySelector("#wind")
 var currentHumidEl = document.querySelector("#humid")
 var currentIconEl = document.querySelector("#current-icon")
-var searchedCities = []
+var searchHistory = [];
 
-if (localStorage.getItem("search-history")){
-    searchedCities = JSON.parse(localStorage.getItem("search-history"))
-} else {
-    localStorage.setItem("search-history",JSON.stringify(searchedCities))
-}
-function getCurrent(cityInput) {
-
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=6a7eff456050fd1c3c03b704bec2225b&units=imperial";
-
+// This function fetches the current weather and appends the data from response to text content on the page.
+function getCurrent(cityName) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=6a7eff456050fd1c3c03b704bec2225b&units=imperial";
 
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
@@ -33,14 +27,16 @@ function getCurrent(cityInput) {
     })
 }
 
-function getForecast(cityInput) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=6a7eff456050fd1c3c03b704bec2225b&units=imperial"
+// This function fetches weather for 5 day forecast and appends the data from response to text content on the page.
+function getForecast(cityName) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=6a7eff456050fd1c3c03b704bec2225b&units=imperial"
 
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data)
-
+                // This for loop steps through the data and gives the value a key
+                // Using currentSelector applies the key value as text to the page
                 for (var i = 0; i < 5; i++) {
                     var singleDay = data.list[i * 8];
                     var forecastWeather = {
@@ -51,7 +47,6 @@ function getForecast(cityInput) {
                         humidity: singleDay.main.humidity
                     }
                     console.log("expected humidity", forecastWeather.humidity)
-
                     var currentSelector = "#day-" + i;
                     document.querySelector(currentSelector).textContent = forecastWeather.date;
                     currentSelector = "#img-" + i;
@@ -63,28 +58,61 @@ function getForecast(cityInput) {
                     currentSelector = "#hum-" + i;
                     document.querySelector(currentSelector).textContent = "Humidity: " + forecastWeather.humidity + "%";
                 }
-
-
             })
         }
     })
 }
 
-function getWeather(event) {
+// Fetch weather for the cityName string
+function onSearchHistoryClick(event, cityName) {
     event.preventDefault();
-    var cityInput = document.querySelector("#cityName").value;
-    let cityHistory = JSON.parse(localStorage.getItem("search-history"));
-    cityHistory.push(cityInput)
-    console.log("city-history", cityHistory);
-    localStorage.setItem("search-history", JSON.stringify(cityHistory));
-    for (var i = 0; i < cityHistory.length; i++) {
-        
-    }
-    getCurrent(cityInput);
-    getForecast(cityInput);
+
+    getCurrent(cityName);
+    getForecast(cityName);
 }
 
-searchButtonEl.addEventListener('click', getWeather);
+// Function that accepts city name string and returns a li with a button that has onclick event that fetches weather.
+function createSearchHistoryListItem(cityName) {
+    let searchBtn = document.createElement("button");
+    let li = document.createElement("li");
+    searchBtn.textContent = cityName;
+    searchBtn.addEventListener("click", (event) => onSearchHistoryClick(event, cityName));
+    li.appendChild(searchBtn);
+
+    return li;
+}
+
+// Pass the search input's value to the searchHistory array as a string.
+// Update local storage to equal searchHistory
+// Create a list item for the search input's value.
+// Fetch weather for the search input's value
+function onSearchSubmit(event) {
+    event.preventDefault();
+    var cityName = document.querySelector("#cityName").value;
+    searchHistory.unshift(cityName);
+    console.log("searchHistory", searchHistory);
+    localStorage.setItem("search-history", JSON.stringify(searchHistory));
+    var li = createSearchHistoryListItem(cityName);
+    cityListEl.insertBefore(li, cityListEl.firstChild);
+
+    getCurrent(cityName);
+    getForecast(cityName);
+}
+
+// Check if search-history exists in local storage.
+// If it does set searchHistory to search-history from local storage then walk through searchHistory and create button and list item and append to cityListEl.
+// Otherwise initialize local storage.
+if (localStorage.getItem("search-history")) {
+    searchHistory = JSON.parse(localStorage.getItem("search-history"));
+    for (var i = 0; i < searchHistory.length; i++) {
+        var li = createSearchHistoryListItem(searchHistory[i]);
+        cityListEl.appendChild(li);
+    }
+} else {
+    localStorage.setItem("search-history", "");
+}
+
+searchButtonEl.addEventListener('click', onSearchSubmit);
 
 
 
